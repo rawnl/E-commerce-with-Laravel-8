f572855b27c395f0d292c39abc7fe143497d437b
rawnl <rawnaq.naili@univ-constantine2.dz>
2021-06-04 15:38:23 +0100

    Initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4 login">
+            <form class="row g-3">
+                <div class="col-12">
+                  <label for="inputEmail4" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="12">
+                  <label for="inputPassword4" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="inputPassword4">
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                      Check me out
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+              </form>            
+        </div>
+    </div>
+</div>
+@endsection
