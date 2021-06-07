9b67bcf1be1cc5edb811904a3c232182197334d4
rawnl <rawnaq.naili@univ-constantine2.dz>
2021-06-07 18:02:45 +0100

    Admin delete product function updated

--- resources/views/productCrud.blade.php
+++ resources/views/productCrud.blade.php
@@ -30,51 +30,18 @@
               </a>
             </li>
           </ul>
-        <!--
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a class="d-flex align-items-center text-muted" href="#">
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Current month
-              </a>
-            </li>
-          </ul>
-        -->
         </div>
       </nav>
 
-      <!--<div class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
-              <button class="btn btn-sm btn-outline-secondary">Share</button>
-              <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar"></span>
-              This week
-            </button>
-          </div>
-        </div>
-        -->
-        
-        
         
         <div class="table-responsive">
           <div class="col-sm-6">
             <h2>Produits</h2>  
             <p>
               <!-- Button trigger modal -->
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModalScrollable">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addModalScrollable">
                 Ajouter un nouveau produit
               </button>
-              <!--<a href="" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" role="button">Ajouter un nouveau  produit</a>-->
             </p> 
           </div>
 
@@ -109,10 +76,12 @@
     
                           <div class="col-sm-4">
                             <p class="text-left">
-                                <a href="deleteProduct/{{$item->id}}" class="btn btn-danger" role="button">Supprimer</a>
+                              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal" onclick="passId(this.id)" id="deleteProduct/{{ $item->id }}">
+                                Supprimer
+                              </button>
                             </p>
                           </div>
-
+                        
                         </td>
                     </tr>  
                     @endforeach                
@@ -128,8 +97,8 @@
 </div>
 
 
-<!-- Modal -->
-<div class="modal fade" id="editModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+<!-- Add Modal -->
+<div class="modal fade" id="addModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -182,4 +151,35 @@
     </div>
   </div>
 </div>
+
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Suppression</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Cette action irréversible. Voulez vous vraiment supprimer ce produit ? 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+        <a class="btn btn-danger" id="deleteByID" role="button">Oui</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+
+  function passId(url){
+    document.getElementById("deleteByID").setAttribute("href", url );
+  }
+
+</script>
+
 @endsection
